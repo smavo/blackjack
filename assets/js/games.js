0b1e1170284496f2bba61c4992d5c9061fb67420
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-24 23:47:57 -0500

    code minify

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,7 +1,7 @@
 /* 2C = Two of Clubs, 2D = Two of Diamonds, 2H = Two of Hearts, 2S = Two of Spades */
 
 const modulo = (() => {
-  "use strict";
+  'use strict';
 
   let deck = [];
   const tipos = ["C", "D", "H", "S"],
